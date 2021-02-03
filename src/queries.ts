0233258d2861c20939c53f30e955220749bd77b6
Lola <lola.donval@gmail.com>
2021-02-03 18:50:09 +0100

    created mutation queries to createRender and createCorrection requests

--- src/queries.ts
+++ src/queries.ts
@@ -19,3 +19,25 @@ export const CREATE_TASK = gql`
     }
   }
 `;
+
+export const CREATE_RENDER = gql`
+  mutation CreateRender($input: InputRender!) {
+    createRender(input: $input) {
+      _id
+      url
+      task
+      user
+    }
+  }
+`;
+
+export const CREATE_CORRECTION = gql`
+  mutation CreateCorrection($input: InputCorrection!) {
+    createCorrection(input: $input) {
+      _id
+      task
+      url
+      user
+    }
+  }
+`;
